0e599726686efc71423f8be28b19f6be21d3c1d1
s1dny <aiden@plarza.com>
2026-03-14 15:28:04 +1100

    feat: add PKG_CONFIG_PATH and LIBCLANG_PATH to environment variables in homelab-module.nix

--- nixos/homelab-module.nix
+++ nixos/homelab-module.nix
@@ -125,11 +125,14 @@ in
     '')
   ];
 
+  environment.variables = {
+    PKG_CONFIG_PATH = lib.makeSearchPath "lib/pkgconfig" [ pkgs.libxml2.dev ];
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+  };
+
   environment.sessionVariables = {
     KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
     CLUSTER = config.networking.hostName;
-    PKG_CONFIG_PATH = lib.makeSearchPath "lib/pkgconfig" [ pkgs.libxml2.dev ];
-    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
   };
 
   sops.age = {
